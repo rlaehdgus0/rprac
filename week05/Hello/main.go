01618086f94a47a6a8cebe58f4d78477cfd4a0e2
KaL <KaL1222kl@gmail.com>
2025-09-22 18:22:49 +0900

    v0.2 Hello/main.go & week05.txt update

--- week05/Hello/main.go
+++ week05/Hello/main.go
@@ -1,8 +1,10 @@
-package main
+package main // 패키지를 main에 속하게 함
 
-import "fmt"
+import "fmt" // fmt라는 모듈을 임포트
 
-func main() {
-	name := "Go"
-	fmt.Println("Hello", name)
+func main() { // 함수 생성
+	// name := "Go"               // name 이라는 변수에 "Go"라는 문자 저장
+	// fmt.Println("Hello", name) // fmt를 이용하여 출력, ,을 통해서 변수와 문자열 구분
+	number := 7
+	fmt.Printf("Number is %d\n", number)
 }
