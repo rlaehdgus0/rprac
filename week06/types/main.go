23eeca61f2bedb004e455df87b101f78a1e0512a
김동현 <forgotten0415@gmail.com>
2025-09-29 18:13:23 +0900

    6번째: .gitignore

--- week06/types/main.go
+++ week06/types/main.go
@@ -6,16 +6,10 @@ import (
 )
 
 func main() {
-	//Zero values
+	//대문자는 외부에서 참조 가능
 	var f64 float64
-	var t bool
-	var s string
-	var i int
-	var i16 int16
+	totalPrice := 1000
 
+	fmt.Println(totalPrice)
 	fmt.Println(f64, reflect.TypeOf(f64))
-	fmt.Println(t, reflect.TypeOf(t))
-	fmt.Println(s, reflect.TypeOf(s))
-	fmt.Println(i, reflect.TypeOf(i))
-	fmt.Println(i16, reflect.TypeOf(i16))
 }
