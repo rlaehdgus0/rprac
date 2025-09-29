a714e1f2cb071c35ab37b7215c319944022b0784
김동현 <forgotten0415@gmail.com>
2025-09-29 17:51:55 +0900

    5번째: zero value

--- week06/types/main.go
+++ week06/types/main.go
@@ -6,26 +6,16 @@ import (
 )
 
 func main() {
-	// var id int16
-	// var name string
-	// var gpa float32
+	//Zero values
+	var f64 float64
+	var t bool
+	var s string
+	var i int
+	var i16 int16
 
-	// id = 999
-	// name = "kim inha"
-	// gpa = 3.99
-
-	// var id int16 = 999
-	// var name string =  "Kim Inha"
-	// var gpa float32 = 3.99
-
-	// var id = 999
-	// var name = "Kim Inha"
-	// var gpa = 3.99
-
-	id := 999
-	name := "Kim Inha"
-	gpa := 3.99
-
-	fmt.Println("학번은", id, reflect.TypeOf(id), ", 이름은", name, reflect.TypeOf(name))
-	fmt.Println("평점 :", gpa, reflect.TypeOf(gpa))
+	fmt.Println(f64, reflect.TypeOf(f64))
+	fmt.Println(t, reflect.TypeOf(t))
+	fmt.Println(s, reflect.TypeOf(s))
+	fmt.Println(i, reflect.TypeOf(i))
+	fmt.Println(i16, reflect.TypeOf(i16))
 }
