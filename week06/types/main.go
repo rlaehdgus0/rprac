e55a477a33e901aa86b3e9d389f75d8c5bf3983c
김동현 <forgotten0415@gmail.com>
2025-09-29 17:08:41 +0900

    dsjkfsjhfahjdf

--- week06/types/main.go
+++ week06/types/main.go
@@ -10,4 +10,6 @@ func main() {
 	fmt.Println(math.Floor(2.21))
 	fmt.Println(math.Ceil(2.21))
 	fmt.Println(strings.Title("go developer!"))
+	fmt.Println("Kim\nInha\t\"\\")
+	fmt.Println("2", "가")
 }
