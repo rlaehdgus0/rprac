37d39f44c9c3d0f12a0a55bead48269b4a25c86e
김동현 <forgotten0415@gmail.com>
2025-09-29 16:56:08 +0900

    first commit

---
+++ week06/types/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"strings"
+)
+
+func main() {
+	fmt.Println(math.Floor(2.21))
+	fmt.Println(math.Ceil(2.21))
+	fmt.Println(strings.Title("go developer!"))
+}
